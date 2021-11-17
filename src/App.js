1ce328d0c2fa8ce25bcb80161f9dfa185fd9e4b5
rzh150030 <rzhuangsg115@gmail.com>
2021-11-17 10:06:14 -0600

    create canvas component with some text

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import {PaintCanvas} from "./components/PaintCanvas/PaintCanvas.js";
 
 function App() {
   return (
     <div className="App">
-       
+      <PaintCanvas />
     </div>
   );
 }
---
+++ src/components/PaintCanvas/PaintCanvas.js
@@ -0,0 +1,9 @@
+import React, {useState} from "react";
+
+export const PaintCanvas = () => {
+    return (
+        <div>
+            FFFFFFFFF
+        </div>
+    )
+}
