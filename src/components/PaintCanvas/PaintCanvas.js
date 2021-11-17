7fbb4841d53e7d0df159853a48cee1da2bf4ee6f
rzh150030 <rzhuangsg115@gmail.com>
2021-11-17 11:32:32 -0600

    create mousedown event handler

--- src/components/PaintCanvas/PaintCanvas.js
+++ src/components/PaintCanvas/PaintCanvas.js
@@ -2,19 +2,32 @@ import React, {useRef, useEffect} from "react";
 
 export const PaintCanvas = () => {
     const canvasRef = useRef(null);
+    const mouse = {x: 0, y: 0};
+
+    const handleMouseDown = () => {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext("2d");
+        ctx.beginPath();
+        ctx.moveTo(mouse.x, mouse.y);
+
+        
+    }
 
     useEffect(() => {
         const canvas = canvasRef.current;
         const ctx = canvas.getContext("2d");
-        ctx.fillStyle = '#000000'
+        ctx.fillStyle = '#000000';
         ctx.canvas.width = window.innerWidth - 100;
         ctx.canvas.height = window.innerHeight;
-        ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-    }, [])
+        ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        ctx.lineJoin = "round";
+        ctx.lineCap = "round";
+        ctx.strokeStyle = "white";
+    }, []);
 
     return (
         <div id="sketch">
-            <canvas ref={canvasRef} />
+            <canvas ref={canvasRef} onMouseDown={handleMouseDown}/>
         </div>
     )
 }
