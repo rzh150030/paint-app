cffe512080899ff34284efaf626057e773f49df3
rzh150030 <rzhuangsg115@gmail.com>
2021-11-17 10:47:49 -0600

    set canvas size using black rectangle

--- src/components/PaintCanvas/PaintCanvas.js
+++ src/components/PaintCanvas/PaintCanvas.js
@@ -1,9 +1,18 @@
-import React, {useState} from "react";
+import React, {useRef, useEffect} from "react";
 
 export const PaintCanvas = () => {
+    const canvasRef = useRef(null);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext("2d");
+        ctx.fillStyle = '#000000'
+        ctx.canvas.width = window.innerWidth - 100;
+        ctx.canvas.height = window.innerHeight;
+        ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+    }, [])
+
     return (
-        <div>
-            FFFFFFFFF
-        </div>
+        <canvas ref={canvasRef} />
     )
 }
