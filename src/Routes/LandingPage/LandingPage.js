78eaa4ffef3f7d1c4d8b5ec1ee7a69d42213e79e
rzh150030 <rzhuangsg115@gmail.com>
2021-11-19 13:51:19 -0600

    a simple about section made

--- src/Routes/LandingPage/LandingPage.js
+++ src/Routes/LandingPage/LandingPage.js
@@ -11,8 +11,9 @@ export default function LandingPage() {
                 <Link to='/canvas' id="canvas-link">Try it out</Link>
             </section>
             <section className="landpage-about">
-                <h2>About</h2>
-                <p>This is a simple little paint app where users can draw pictures either through free draws or shapes. Take a look and see what you can make!</p>
+                <h2 id="about-label">About</h2>
+                <p>This is a simple little paint app where users can draw pictures either through free drawing or
+                    by clicking out shapes to form a complete picture. Take a look and see what you can make!</p>
             </section>
         </div>
     )
