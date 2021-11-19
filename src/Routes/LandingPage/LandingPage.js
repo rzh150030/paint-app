a5eb145e530836104ecd1f92030fa25719249b90
rzh150030 <rzhuangsg115@gmail.com>
2021-11-19 09:59:48 -0600

    add a welcome message and button to the paint canvas

--- src/Routes/LandingPage/LandingPage.js
+++ src/Routes/LandingPage/LandingPage.js
@@ -2,12 +2,16 @@ import { Link } from 'react-router-dom';
 import './LandingPage.css';
 
 export default function LandingPage() {
+
+
     return (
         <div>
-            <h1> LandingPage </h1>
-            <Link to='/canvas'>
-                Go To Canvas
-            </Link>
+            <section className="landpage-introduction">
+                <h2>Welcome to a simple paint app!</h2>
+                <Link to='/canvas'>
+                    <button>Try it out</button>
+                </Link>
+            </section>
         </div>
     )
 }
