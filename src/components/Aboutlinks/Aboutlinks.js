51d2e4dcead4dc823694328da840502a2a049448
rzh150030 <rzhuangsg115@gmail.com>
2021-11-19 14:01:04 -0600

    create an about component for footer links to github profile

---
+++ src/components/Aboutlinks/Aboutlinks.js
@@ -0,0 +1,11 @@
+
+export default function() {
+    return (
+        <footer className="about-links">
+            <p className="author-credits">Developed by:
+                <a className="authors" href="https://github.com/rzh150030">Richard Huang</a>
+                <a className="authors" href="https://github.com/Rzavala414">Rogelio Zavala</a>
+            </p>
+        </footer>
+    )
+}
