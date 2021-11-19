6c97d7c933001c34adf59ae8719af999a2340be6
rzh150030 <rzhuangsg115@gmail.com>
2021-11-19 14:22:34 -0600

    created and styled footer

--- src/components/AboutLinks/AboutLinks.js
+++ src/components/AboutLinks/AboutLinks.js
@@ -1,11 +1,11 @@
-import "./Aboutlinks.css";
+import "./AboutLinks.css";
 
 export default function AboutLinks() {
     return (
         <footer className="about-links">
             <p className="author-credits">Developed by:
                 <a className="authors" href="https://github.com/rzh150030">Richard Huang</a>
-                <a className="authors" href="https://github.com/Rzavala414">Rogelio Zavala</a>
+                <a className="authors" href="https://github.com/Rzavala414">Roger Zavala</a>
             </p>
         </footer>
     )
