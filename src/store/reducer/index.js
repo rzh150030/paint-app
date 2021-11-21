d80d111f0c5d9944c0ff0d42ccc961cc716c5e4f
rzh150030 <rzhuangsg115@gmail.com>
2021-11-21 16:05:51 -0600

    add undo action for store

--- src/store/reducer/index.js
+++ src/store/reducer/index.js
@@ -1,9 +1,16 @@
 const ADD_COORD = "ADD_COORD";
+const UNDO_DRAW = "UNDO_DRAW";
 
 export function addCoordAction(image) {
     return {
         type: ADD_COORD,
         drawCoords: image
+    };
+}
+
+export function undoAction() {
+    return {
+        type: UNDO_DRAW
     };
 }
 
@@ -14,6 +21,9 @@ export default function reducer(state = initialState, action) {
         case ADD_COORD:
             const newState = {...state, images: [...state.images, action.drawCoords]};
             return newState;
+        case UNDO_DRAW:
+            state.images.pop();
+            return {...state, images: [...state.images]};
         default:
             return state;
     }
