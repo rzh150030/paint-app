0373266a59424b26afb94c7f363c0d80eedeaa5b
rzh150030 <rzhuangsg115@gmail.com>
2021-11-22 15:10:59 -0600

    clear canvas to redraw old coordinates later

--- src/components/Undo/Undo.js
+++ src/components/Undo/Undo.js
@@ -3,18 +3,43 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useSelector, useDispatch } from 'react-redux';
 import { undoAction } from "../../store/reducer";
 
-export default function Undo({getColor, color}) {
+export default function Undo({clearCanvas}) {
     const dispatch = useDispatch();
     const images = useSelector(state => state.images);
     const canvas = useSelector(state => state.canvas);
 
     const handleUndo = () => {
         if (images.length) {
+
             const ctx = canvas.getContext("2d");
+            clearCanvas();
+            dispatch(undoAction());
+            debugger
+            ctx.beginPath();
+            ctx.moveTo(images[0].x, images[0].y);
+
+            for (let i = 1; i < images.length; i++) {
+                const coord = images[i];
+                ctx.lineJoin = "round";
+                ctx.lineCap = "round";
+                ctx.lineTo(coord.x, coord.y);
+                ctx.stroke();
+            }
+        }
+    }
+
+    return (
+        <div>
+           <div>Undo</div>
+           <FontAwesomeIcon icon={faUndo} onClick={handleUndo}/>
+        </div>
+    )
+}
+
+
+/* const ctx = canvas.getContext("2d");
             const eraseCoords = images[images.length - 1];
             const currColor = color;
-            // getColor('#000');
-            // getColor("#4287f5");
             ctx.strokeStyle = "#000000";
             ctx.beginPath();
             ctx.moveTo(eraseCoords[0].x, eraseCoords[0].y);
@@ -28,14 +53,4 @@ export default function Undo({getColor, color}) {
                 ctx.lineTo(coord.x, coord.y);
                 ctx.stroke();
             }
-            ctx.strokeStyle = currColor;
-        }
-    }
-
-    return (
-        <div>
-           <div>Undo</div>
-           <FontAwesomeIcon icon={faUndo} onClick={handleUndo}/>
-        </div>
-    )
-}
+            ctx.strokeStyle = currColor; */
